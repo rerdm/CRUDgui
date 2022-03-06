6ca9b4ffbdf297045b98cd9b80b5cac20c009b5e
renee <reneerdmann87@web.de>
2022-03-06 20:34:59 +0100

    second commit

---
+++ CRUDgui/src/programms/prog.java
@@ -0,0 +1,16 @@
+package programms;
+
+import javax.swing.JFrame;
+
+public class prog {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(200, 200);
+		
+		frame.setVisible(true);
+		
+	}
+	
+}
